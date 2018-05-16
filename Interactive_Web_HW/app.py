e2827121148db4c19341b003f268bca39aaa20f3
Irene Chang <irenelanrrchag@hotmail.com>
2018-05-16 09:12:07 -0500

    updated HW upload

--- Interactive_Web_HW/app.py
+++ Interactive_Web_HW/app.py
@@ -61,10 +61,12 @@ def home():
     # ]
 
 def names():    
-    results = session.query(samples)
+    results = session.query(Samples)
     list_names = list(session.execute(results).keys())
     full_name_list = list_names[1:]
-    return jsonify(full_name_list)
+    stripped_list = [s.strip('samples_') for s in full_name_list]
+    return jsonify(stripped_list)
+    # return jsonify(list_names)
 
 
 @app.route("/otu")
@@ -99,27 +101,33 @@ def otu_list():
     # }
     # """
 def metadata(sample):
-    results = session.query(Metadata.AGE, Metadata.BBTYPE, Metadata.ETHNICITY, Metadata.GENDER, Metadata.LOCATION, Metadata.SAMPLEID).filter(Metadata.SAMPLEID == "BB_" + sample)
-    metadata_list = []
-    for result in results:
-        row = {}
-        row["Age"] = result[0]
-        row["BBType"] = result[1]
-        row["Ethnicity"] = result[2]
-        row["Gender"] = result[3]
-        row["Location"] = result[4]
-        row["SampleID"] = result[5]
-        metadata_list.append(row)
+    results = session.query(Metadata.AGE, Metadata.BBTYPE, Metadata.ETHNICITY, Metadata.GENDER, Metadata.LOCATION, Metadata.SAMPLEID).filter(Metadata.SAMPLEID == "BB_" + sample).all()
+    result = list(results)
+    # result = results[0]
+    metadata_list = {}
+    for r in results:
+        metadata_list = {
+        "Age" : r[0],
+        "BBType" : r[1],
+        "Ethnicity" : r[2],
+        "Gender" : r[3],
+        "Location" : r[4],
+        "SampleID" : r[5]
+        }
+            
     return jsonify(metadata_list)
+    
+    
 
 # Returns an integer value for the weekly washing frequency `WFREQ`
 @app.route('/wfreq/<sample>')
 def sample_wfreq(sample):
     """Return the Weekly Washing Frequency as a number."""
-    sampleID=int(sample.split("_")[1])
+    sampleID=(sample.split("_")[1])
     results = session.query(Metadata.WFREQ).\
         filter(Metadata.SAMPLEID == sampleID).first()
-    wfreq = int(results)
+    for result in results:
+        wfreq = int(result)
     # Return only the first integer value for washing frequency
     return jsonify(wfreq)
 
@@ -143,7 +151,7 @@ def sample_wfreq(sample):
     # ]
     # """
 def otu_id_values(sample):
-    results = session.execute("SELECT otu_id, "+sample+" FROM samples ORDER BY "+sample+" DESC").fetchall
+    results = session.execute(f"SELECT otu_id, {sample} FROM samples ORDER BY {sample} DESC").fetchall()
     # Create lists of otu_ids and values
     otus = [item[0] for item in results]
     values = [item[1] for item in results]  
